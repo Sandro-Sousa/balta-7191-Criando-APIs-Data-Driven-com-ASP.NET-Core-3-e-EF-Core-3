289de403d43bf13e109012c7a0c5a2d963bf6591
Sandro-Sousa <sandro-souza-oliveira@hotmail.com>
2020-10-13 19:34:39 -0300

    Finalizado a API

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Backoffice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
